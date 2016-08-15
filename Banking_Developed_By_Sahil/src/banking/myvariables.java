ec092d9addc9f4ea58a1c927a53d5b78dffdc5fa
Sahil Sodhi <sahilsodhi18@yahoo.com>
2016-08-15 18:22:44 +0530

    Banking Application

---
+++ Banking_Developed_By_Sahil/src/banking/myvariables.java
@@ -0,0 +1,13 @@
+
+
+package banking;
+
+
+public interface myvariables 
+{
+    String path = "jdbc:mysql://localhost/";
+    String place = "bank_records";
+    String username = "root";
+    String password = "";
+       
+}
